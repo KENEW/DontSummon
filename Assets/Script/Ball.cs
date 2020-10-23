80aebecd7a14fc33da14b840a67705c682c754e8
Seunghyun <whtmdgus408@gmail.com>
2020-10-24 05:18:09 +0900

    폴더들 추가 / 인 게임 씬 테스트

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rigid;
+
+    [SerializeField]
+    private Vector2 curDir = new Vector2(1f, 0.5f);
+
+    private float moveSpeed = 200f;
+
+    void Start()
+    {
+        //rigid.AddForce(curDir * moveSpeed * Time.deltaTime, ForceMode2D.Impulse);
+        rigid.velocity = new Vector2(4, 2);
+    }
+
+    Vector2 tempVelo;
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        //if (collision.gameObject.CompareTag("Ball"))
+        //{
+        //    rigid.velocity = Vector3.zero;
+		//}
+	}
+
+}
---
+++ Assets/Script/BallSpawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawn : MonoBehaviour
+{
+    public GameObject ballPre;
+
+    [SerializeField]
+    private new Vector2 startPos = new Vector2(0, 0);
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(ballPre, startPos, Quaternion.identity);
+		}
+    }
+}
