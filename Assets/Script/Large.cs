f31ffe51c4a9f5350988fbbddacefee0ce8183d1
slow-cosmos <inki1030@naver.com>
2020-10-24 13:14:35 +0900

    test Scene - 랜덤 위치 (불안정하네용..) - 소환진을 향해 이동 (튕겨야 하는데 이것도 고치는 중입니다)

---
+++ Assets/Script/Large.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Large : MonoBehaviour
+{
+    public Rigidbody2D rigid;
+
+    [SerializeField]
+    private Vector2 curDir = new Vector2(1f, 0.5f);
+
+    private float moveSpeed = 0.04f;
+
+    private Vector2 portal = new Vector2(0.0f, 0.0f);
+
+    void Update()
+    {
+        Vector2 position = Vector2.MoveTowards(transform.position, portal, moveSpeed);
+        rigid.MovePosition(position);
+    }
+
+
+    Vector2 tempVelo;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.gameObject.CompareTag("Ball"))
+        //{
+        //    rigid.velocity = Vector3.zero;
+        //}
+    }
+
+}
---
+++ Assets/Script/MonsterGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterGenerator : MonoBehaviour
+{
+    public GameObject smallPre;
+    public GameObject mediumPre;
+    public GameObject largePre;
+    public GameObject[] monsterObject;
+
+    private float radius = 5.0f; //원안의 랜덤 위치
+    private int smallNum = 3;
+    private int mediumNum = 2;
+    private int largeNum = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        for(int i=0; i<smallNum;i++) //소
+        {
+            Instantiate(smallPre, RandomPosition(radius), Quaternion.identity);
+        }
+
+        for(int i=0;i<mediumNum;i++) //중
+        {
+            Instantiate(mediumPre, RandomPosition(radius), Quaternion.identity);
+        }
+
+        for(int i=0;i<largeNum;i++) //대
+        {
+            Instantiate(largePre, RandomPosition(radius), Quaternion.identity);
+        }
+
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public Vector2 RandomPosition(float radius) //랜덤 위치
+    {
+        //Vector2 getPoint = Random.insideUnitCircle;
+        //float r = Random.Range(0.0f, radius);
+        //return (getPoint * r) + transform.position;
+        int signX = 1;
+        int signY = 1;
+
+        Vector2 randomPos = Random.insideUnitCircle * radius;
+
+        if(randomPos.x<2.5f && randomPos.y<2.5f) //원반 위에 있게
+        {
+            if(randomPos.x > -2.5f && randomPos.y > -2.5f)
+            {
+                if(randomPos.x<0)
+                {
+                    signX = -1;
+                }
+                if(randomPos.y<0)
+                {
+                    signY = -1;
+                }
+                randomPos.x += 2.5f * signX;
+                randomPos.y += 2.5f * signY;
+            }
+            
+        }
+
+        return randomPos;
+    }
+}
