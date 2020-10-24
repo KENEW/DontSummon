ce2a4465b0ad19711f230ebfd687f3754496006a
slow-cosmos <inki1030@naver.com>
2020-10-24 16:27:04 +0900

    클릭시 튕김 구현 (불안정한 것 같아서 보완하겠습니당)

---
+++ Assets/Script/InGame/Monster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public Rigidbody2D rigid;
+    private float moveSpeed;
+
+    void Start()
+    {
+        if(transform.CompareTag("Small"))
+        {
+            moveSpeed = 0.8f;
+        }
+        else if(transform.CompareTag("Medium"))
+        {
+            moveSpeed = 0.6f;
+        }
+        else
+        {
+            moveSpeed = 0.4f;
+        }
+
+        rigid.velocity = new Vector2(-rigid.transform.position.x * moveSpeed, -rigid.transform.position.y * moveSpeed);
+    }
+
+    void OnMouseDown()
+    {
+        rigid.velocity = new Vector2(rigid.transform.position.x * moveSpeed, rigid.transform.position.y * moveSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        Debug.Log("소환");
+        Destroy(gameObject);
+    }
+}
