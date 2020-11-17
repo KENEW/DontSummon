da73b94fdb84a10bee28f1320087e501d0594a75
DESKTOP-MLUG35V\juring <inki1030@naver.com>
2020-11-17 22:18:14 +0900

    editScene 추가 및 변경 스크립트는 수정 안하고 NewInGame에 새로 추가했어요 !!!

---
+++ Assets/Script/NewInGame/Bullet.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody2D rigid;
+    private SpriteRenderer renderer;
+
+    [SerializeField]
+    private Vector2 curDir = new Vector2(1f, 0.5f);
+
+    private float moveSpeed=0.2f;
+    private float power;
+
+
+    void Start()
+    {
+        renderer = gameObject.GetComponent<SpriteRenderer>();
+
+        //포탈을 향해 이동
+        rigid.velocity = new Vector2(-rigid.transform.position.x * moveSpeed, -(rigid.transform.position.y+4.27f) * moveSpeed);
+
+    }
+
+   
+    public void SetPower(float powerValue)
+    {
+        power = powerValue;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D coll) //포탈이나 몬스터에 닿으면 destroy
+    {
+        SoundManager.Instance.PlaySFX("Tick");
+
+        if (coll.gameObject.tag == "Portal")
+        {
+            Destroy(gameObject);
+        }
+
+        if (coll.gameObject.tag == "RedMonster")
+        {
+            Destroy(gameObject);
+        }
+
+        if (coll.gameObject.tag == "GreenMonster")
+        {
+            Destroy(gameObject);
+        }
+
+        if (coll.gameObject.tag == "BlueMonster")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll) //타일
+    {
+        if(coll.CompareTag("RedTile"))
+        {
+            renderer.color = new Color(1.0f, 0f, 0f);
+        }
+
+        else if (coll.CompareTag("BlueTile"))
+        {
+            renderer.color = new Color(0f, 0f, 1.0f);
+        }
+
+        else if (coll.CompareTag("GreenTile"))
+        {
+            renderer.color = new Color(0f, 1.0f, 0f);
+        }
+
+       
+    }
+}
---
+++ Assets/Script/NewInGame/Monster1.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster1 : MonoBehaviour
+{
+    public GameObject bullet;
+    private SpriteRenderer renderer;
+
+    int monsterHp = 5;
+
+    float timer = 0f;
+    int waitingTime = 5;
+
+    Vector2 bulletPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = gameObject.GetComponent<SpriteRenderer>();
+
+        bulletPos = new Vector2(transform.position.x, transform.position.y - 1.0f);
+        Instantiate(bullet, bulletPos, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(monsterHp>0) //몬스터의 hp가 남아있으면 5초 간격으로 생성
+        {
+            if (timer > waitingTime)
+            {
+                Instantiate(bullet, bulletPos, Quaternion.identity);
+                timer = 0;
+            }
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll) 
+    {
+        /*
+        if(transform.CompareTag("RedMonster"))
+        {
+            if(coll.renderer.color ==  Color(1.0f,0f,0f))
+            {
+                monsterHp -= 1;
+                Debug.Log(monsterHp);
+            }
+        }
+
+        else if (transform.CompareTag("GreenMonster"))
+        {
+            if (coll.renderer.color == new Color(0f, 1.0f, 0f))
+            {
+                monsterHp -= 1;
+                Debug.Log(monsterHp);
+            }
+        }
+
+        else if (transform.CompareTag("BlueMonster"))
+        {
+            if (coll.renderer.color == new Color(0f, 0f, 1.0f))
+            {
+                monsterHp -= 1;
+                Debug.Log(monsterHp);
+            }
+        }
+        */
+        
+
+    }
+
+}
