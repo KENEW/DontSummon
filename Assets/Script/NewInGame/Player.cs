fa776d78d5cfb029c52e005c9b0c02760dc6b246
slow-cosmos <inki1030@naver.com>
2020-11-19 17:40:53 +0900

    터치 구역 수정

---
+++ Assets/Script/NewInGame/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector2 MousePosition;
+    Camera Camera;
+
+    private void Start()
+    {
+        Camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MousePosition = Camera.ScreenToWorldPoint(Input.mousePosition);
+
+            if(MousePosition.x > 2.5f)
+            {
+                MousePosition.x = 2.5f;
+            }
+
+            if (MousePosition.x < -2.5f)
+            {
+                MousePosition.x = -2.5f;
+            }
+
+            if (MousePosition.y > -2.7f)
+            {
+                MousePosition.y = -2.7f;
+            }
+
+            if (MousePosition.y < -4f)
+            {
+                MousePosition.y = -4f;
+            }
+
+            transform.position = MousePosition;
+
+        }
+    }
+
+    
+
+}
