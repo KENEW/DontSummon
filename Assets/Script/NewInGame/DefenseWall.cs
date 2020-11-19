1d5001db3d8c308ad94de642e2ca8093da59c75a
slow-cosmos <inki1030@naver.com>
2020-11-19 18:15:45 +0900

    디펜스 월 구현

---
+++ Assets/Script/NewInGame/DefenseWall.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenseWall : MonoBehaviour
+{
+    public int wallHp;
+    Color color;
+    SpriteRenderer spr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wallHp = 3;
+
+        spr = GetComponent<SpriteRenderer>();
+        color = spr.color;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (wallHp==2)
+        {
+            color.a = 0.66f;
+            spr.color = color;
+        }
+
+        else if (wallHp == 1)
+        {
+            color.a = 0.33f;
+            spr.color = color;
+        }
+
+        else if(wallHp ==0)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll) //공이 닿았을 때
+    {
+        if (coll.gameObject.tag == "RedBullet")
+        {
+            wallHp -= 1;
+        }
+
+        else if (coll.gameObject.tag == "GreenBullet")
+        {
+            wallHp -= 1;
+        }
+
+        else if (coll.gameObject.tag == "BlueBullet")
+        {
+            wallHp -= 1;
+        }
+        
+        else if (coll.gameObject.tag == "Bullet")
+        {
+            wallHp -= 1; 
+        }
+
+    }
+}
