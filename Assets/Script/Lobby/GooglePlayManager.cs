eab5c783653f63527b0539db57c66573cc3c7c4a
Seunghyun <whtmdgus408@gmail.com>
2020-12-11 17:05:12 +0900

    #구글 매니저 플러그인 #GoogleManager Cs 추가 #로그인 테스트 버튼

---
+++ Assets/Script/Lobby/GooglePlayManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+public class GooglePlayManager : MonoBehaviour
+{
+    bool bWait = false;
+    public Text text;
+
+    void Awake()
+    {
+        PlayGamesPlatform.InitializeInstance(new PlayGamesClientConfiguration.Builder().Build());
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+
+        text.text = "no Login";
+    }
+    public void OnLogin()
+    {
+        if (!Social.localUser.authenticated)
+        {
+            Social.localUser.Authenticate((bool bSuccess) =>
+            {
+                if (bSuccess)
+                {
+                    Debug.Log("Success : " + Social.localUser.userName);
+                    text.text = Social.localUser.userName;
+                }
+                else
+                {
+                    Debug.Log("Fall");
+                    text.text = "Fail";
+                }
+            });
+        }
+    }
+
+    public void OnLogOut()
+    {
+        ((PlayGamesPlatform)Social.Active).SignOut();
+        text.text = "Logout";
+    }
+}
