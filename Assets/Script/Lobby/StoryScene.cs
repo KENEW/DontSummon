8978cd72d8db9492c5128e3e28c9005d01efed09
Seunghyun <whtmdgus408@gmail.com>
2020-12-22 17:00:02 +0900

    # Lobby 씬 스테이지 선택 이미지 수정 # Lobby 씬 스토리 패널 추가 # StoryScene.cs 수정

--- Assets/Script/Lobby/StoryScene.cs
+++ Assets/Script/Lobby/StoryScene.cs
@@ -5,24 +5,12 @@ using UnityEngine.Experimental.GlobalIllumination;
 
 public class StoryScene : MonoBehaviour
 {
-	public GameObject[] storyPanel;
 	public GameObject mainTitle;
-
-	private int curPage = 0;
 
 	public void NextButton()
 	{
 		SoundManager.Instance.PlaySFX("Button");
-		storyPanel[curPage].SetActive(false);
-		curPage++;
-
-		if (curPage >= storyPanel.Length)
-		{
-			mainTitle.SetActive(true);
-			gameObject.SetActive(false);
-			return;
-		}
-
-		storyPanel[curPage].SetActive(true);
+		mainTitle.SetActive(true);
+		this.gameObject.SetActive(false);
 	}
 }
